8b74c39e391dcbec89fea9027727571016dbd39c
Rachel Sartori <sartorir@umich.edu>
2017-02-08 17:02:27 -0500

    edited section day/time

--- SI206W17_HW4.py
+++ SI206W17_HW4.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 ## SI 206 - W17 - HW4
 ## COMMENT WITH:
-## Your section day/time:
+## Your section day/time: Thursdays 6-7
 ## Any names of people you worked with on this assignment:
 
 #####################
