1d2994e1b42202b0a708be45e19ff1fb120a7458
Rachel Sartori <sartorir@umich.edu>
2017-02-11 13:25:08 -0500

    solved part two: first ten headlines

--- SI206W17_HW4.py
+++ SI206W17_HW4.py
@@ -44,8 +44,14 @@ fileref.close()
 ## HINT: Remember that you'll need to open the file you created in Part 1, read the contets into one big string, and make a BeautifulSoup object out of that string!
 ## NOTE that the provided link does not include saving the online data in a file as part of the process. But it still provides very useful hints/tricks about how to look for and identify the headlines on the NY Times page.
 
+soup = BeautifulSoup(html_text, 'html.parser')
+nytimes_headlines = []
+headlines = soup.find_all('h2', class_ ="story-heading", limit = 10)
+for x in headlines:
+	stripped_headline = x.text.replace("\n", " ").strip()
+	nytimes_headlines.append(stripped_headline)
 
-
+print (nytimes_headlines)
 
 #####################
 
